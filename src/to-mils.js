fea3e7021ad0ad260ff1444da7ce21b5cf8703fd
Teyler Halama <teyler.halama@gmail.com>
2018-10-06 13:16:29 -0600

    Added & operator, removed reduced function, and added mins and secs
    Added a regex expression in the split method to look for 'and' as well as '&'. I also removed the .reduce method way of doing it for a more simple sum. I also added 'mins' and 'secs' letter checkers.

--- src/to-mils.js
+++ src/to-mils.js
@@ -1,23 +1,20 @@
 module.exports = (input) => {
-    let words = input.split(" and ");
+    let words = input.split(/\s*[&]\s*|\s*and\s*/g);
+    let sum = 0;
+
     for (i=0; i<words.length; i++) {
         let numbers = parseFloat(/[\d\.]+/gm.exec(words[i])[0])
         let letters = (/[A-z ]+/gm.exec(words[i])[0]).replace(/[ ]/gm, '')
 
         if (letters == "day" || letters == "days") {
-            words[i] = numbers * 86400000
+            sum += numbers * 86400000
         } else if (letters == "hr" || letters == "hrs" || letters == "hour" || letters == "hours") {
-            words[i] =  numbers * 3600000
-        } else if (letters == "min" || letters == "minute" || letters == "minutes") {
-            words[i] = numbers * 60000
-        } else if (letters == "sec" || letters == "second" || letters == "seconds") {
-            words[i] =  numbers * 1000
+            sum +=  numbers * 3600000
+        } else if (letters == "min" || letters == "mins" || letters == "minute" || letters == "minutes") {
+            sum += numbers * 60000
+        } else if (letters == "sec" || letters == "secs" || letters == "second" || letters == "seconds") {
+            sum += numbers * 1000
         }
     }
-
-    return words.reduce(getSum);
-}
-
-function getSum(total, num) {
-    return total + num;
+    return sum;
 }
